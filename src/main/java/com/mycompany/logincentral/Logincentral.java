825138ca5ff21e6039ef3877f2c361d2a60b02d6
peter2313 <pedro.garvic@educa.jcyl.es>
2023-12-01 20:33:55 +0100

    primer commit login central

---
+++ src/main/java/com/mycompany/logincentral/Logincentral.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.logincentral;
+
+/**
+ *
+ * @author Pedro Garcia Vicente
+ */
+public class Logincentral {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
